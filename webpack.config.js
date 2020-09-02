35df99521dbdb9eeef7ff7b1251ed43bca64f275
Pieter <oosthuizenp@gmail.com>
2020-09-02 21:41:55 +0200

    merge

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    performance: {
+        hints: false
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    entry: './_assets/bundle.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'assets')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        ]
+    }
+};
