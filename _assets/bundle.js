bfda556bcce04e9083a8ee663e640023117fd8b9
Pieter <oosthuizenp@gmail.com>
2020-09-12 20:58:59 +0200

    Cleanup

--- _assets/bundle.js
+++ _assets/bundle.js
@@ -6,3 +6,4 @@ import '@fortawesome/fontawesome-free/js/all.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './site.css';
 import './css/style.scss';
+import './js/site';
--- bundle.js
+++
@@ -1,7 +0,0 @@
-import 'expose-loader?exposes[]=$&exposes[]=jQuery!jquery';
-import 'expose-loader?exposes[]=Popper!popper.js';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@fortawesome/fontawesome-free/js/all.js';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './site.css';
